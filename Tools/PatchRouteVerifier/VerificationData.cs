bea306327d3cdbab408588f41a4def98906b3e3f
qrer321 <85899638+qrer321@users.noreply.github.com>
2026-05-09 01:16:09 +0900

    refactor: split verifier data constants

---
+++ Tools/PatchRouteVerifier/VerificationData.cs
@@ -0,0 +1,102 @@
+namespace FfxivKoreanPatch.PatchRouteVerifier
+{
+    internal static partial class PatchRouteVerifier
+    {
+        private const string TextPrefix = "0a0000.win32";
+        private const string FontPrefix = "000000.win32";
+        private const string UiPrefix = "060000.win32";
+        private const string DataCenterTitleUldPath = "ui/uld/Title_DataCenter.uld";
+        private const string DataCenterWorldmapUldPath = "ui/uld/Title_Worldmap.uld";
+        private const string DefaultGlyphDumpFolderName = "patch-route-glyph-dumps";
+        private const string LobbyPhraseGlyphGroup = "lobby-phrase";
+        private const string DialoguePhraseGlyphGroup = "dialogue-phrase";
+        private const string LobbyPhraseTargetFontPath = "common/font/AXIS_14_lobby.fdt";
+        private const string LobbyPhraseReferenceFontPath = "common/font/AXIS_12_lobby.fdt";
+        private const int ProtectedHangulMinimumVisiblePixels = 300;
+        private const uint UncompressedBlock = 32000;
+
+        private static readonly string[] LobbyPhraseFontPaths = new string[]
+        {
+            "common/font/AXIS_12_lobby.fdt",
+            "common/font/AXIS_14_lobby.fdt",
+            "common/font/AXIS_18_lobby.fdt",
+            "common/font/AXIS_36_lobby.fdt",
+            "common/font/Jupiter_16_lobby.fdt",
+            "common/font/Jupiter_20_lobby.fdt",
+            "common/font/Jupiter_23_lobby.fdt",
+            "common/font/Jupiter_45_lobby.fdt",
+            "common/font/Jupiter_46_lobby.fdt",
+            "common/font/Jupiter_90_lobby.fdt",
+            "common/font/Meidinger_16_lobby.fdt",
+            "common/font/Meidinger_20_lobby.fdt",
+            "common/font/Meidinger_40_lobby.fdt",
+            "common/font/MiedingerMid_10_lobby.fdt",
+            "common/font/MiedingerMid_12_lobby.fdt",
+            "common/font/MiedingerMid_14_lobby.fdt",
+            "common/font/MiedingerMid_18_lobby.fdt",
+            "common/font/MiedingerMid_36_lobby.fdt",
+            "common/font/TrumpGothic_23_lobby.fdt",
+            "common/font/TrumpGothic_34_lobby.fdt",
+            "common/font/TrumpGothic_68_lobby.fdt",
+            "common/font/TrumpGothic_184_lobby.fdt"
+        };
+
+        private static readonly string[,] Derived4kLobbyFontPairs = new string[,]
+        {
+            { "common/font/AXIS_36_lobby.fdt", "common/font/AXIS_36.fdt" },
+            { "common/font/Jupiter_46_lobby.fdt", "common/font/Jupiter_46.fdt" },
+            { "common/font/Jupiter_90_lobby.fdt", "common/font/Jupiter_46.fdt" },
+            { "common/font/Meidinger_40_lobby.fdt", "common/font/MiedingerMid_36.fdt" },
+            { "common/font/MiedingerMid_36_lobby.fdt", "common/font/MiedingerMid_36.fdt" },
+            { "common/font/TrumpGothic_68_lobby.fdt", "common/font/TrumpGothic_68.fdt" }
+        };
+
+        private static readonly uint[] Derived4kLobbyRequiredHangulCodepoints = new uint[]
+        {
+            0xCE90, 0xB9AD, 0xD130, 0xC815, 0xBCF4, 0xB97C, 0xBCC0, 0xACBD,
+            0xD558, 0xAE30, 0xC704, 0xD574, 0xB85C, 0xC2A4, 0xAC00, 0xB974,
+            0xD2B8, 0xB2C8, 0xBA54, 0xC774, 0xC544, 0xADF8, 0xB9BC, 0xC790,
+            0xB370, 0xC13C, 0xC2DC, 0xD15C, 0xC124, 0xAE00, 0xAF34, 0xD06C,
+            0xD45C, 0xD30C, 0xD2F0, 0xBAA9, 0xB85D, 0xC785, 0xC7A5, 0xC548,
+            0xB0B4, 0xCD08, 0xC2B9, 0xB2EC, 0xB808, 0xBCA8
+        };
+
+        private static readonly uint[] PartyListProtectedPuaGlyphs = new uint[]
+        {
+            0xE031, // self marker
+            0xE037, // Occult Crescent / phantom level
+            0xE0E1, 0xE0E2, 0xE0E3, 0xE0E4,
+            0xE0E5, 0xE0E6, 0xE0E7, 0xE0E8
+        };
+
+        private static readonly string[] DialoguePhraseFontPaths = new string[]
+        {
+            "common/font/AXIS_12.fdt",
+            "common/font/AXIS_14.fdt",
+            "common/font/AXIS_18.fdt",
+            "common/font/AXIS_36.fdt",
+            "common/font/KrnAXIS_120.fdt",
+            "common/font/KrnAXIS_140.fdt",
+            "common/font/KrnAXIS_180.fdt",
+            "common/font/KrnAXIS_360.fdt",
+            "common/font/Jupiter_16.fdt",
+            "common/font/Jupiter_20.fdt",
+            "common/font/Jupiter_23.fdt",
+            "common/font/Jupiter_45.fdt",
+            "common/font/Jupiter_46.fdt",
+            "common/font/Jupiter_90.fdt",
+            "common/font/Meidinger_16.fdt",
+            "common/font/Meidinger_20.fdt",
+            "common/font/Meidinger_40.fdt",
+            "common/font/MiedingerMid_10.fdt",
+            "common/font/MiedingerMid_12.fdt",
+            "common/font/MiedingerMid_14.fdt",
+            "common/font/MiedingerMid_18.fdt",
+            "common/font/MiedingerMid_36.fdt",
+            "common/font/TrumpGothic_23.fdt",
+            "common/font/TrumpGothic_34.fdt",
+            "common/font/TrumpGothic_68.fdt",
+            "common/font/TrumpGothic_184.fdt"
+        };
+    }
+}
