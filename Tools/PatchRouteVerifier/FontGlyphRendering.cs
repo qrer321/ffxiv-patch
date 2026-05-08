8824d457ef21e866a10fb6251eac3108099110d9
qrer321 <85899638+qrer321@users.noreply.github.com>
2026-05-09 01:46:24 +0900

    refactor: split fdt glyph parsing

--- Tools/PatchRouteVerifier/FontGlyphRendering.cs
+++ Tools/PatchRouteVerifier/FontGlyphRendering.cs
@@ -1,9 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.IO.Compression;
-using FfxivKoreanPatch.FFXIVPatchGenerator;
 
 namespace FfxivKoreanPatch.PatchRouteVerifier
 {
@@ -24,11 +22,6 @@ namespace FfxivKoreanPatch.PatchRouteVerifier
         private const string FontLobby6TexturePath = "common/font/font_lobby6.tex";
         private const string FontLobby7TexturePath = "common/font/font_lobby7.tex";
         private const string FontKrnTexturePath = "common/font/font_krn_1.tex";
-        private const int FdtHeaderSize = 0x20;
-        private const int FdtFontTableHeaderSize = 0x20;
-        private const int FdtGlyphEntrySize = 0x10;
-        private const int FdtKerningHeaderSize = 0x10;
-        private const int FdtKerningEntrySize = 0x10;
         private const int GlyphCanvasSize = 96;
 
         private sealed partial class Verifier
@@ -143,188 +136,6 @@ namespace FfxivKoreanPatch.PatchRouteVerifier
             }
 
             return canvas;
-        }
-
-        private static bool GlyphSpacingMetricsMatch(FdtGlyphEntry sourceGlyph, FdtGlyphEntry targetGlyph)
-        {
-            return sourceGlyph.ShiftJisValue == targetGlyph.ShiftJisValue &&
-                   sourceGlyph.Width == targetGlyph.Width &&
-                   sourceGlyph.Height == targetGlyph.Height &&
-                   sourceGlyph.OffsetX == targetGlyph.OffsetX &&
-                   sourceGlyph.OffsetY == targetGlyph.OffsetY;
-        }
-
-        private static string FormatGlyphSpacing(FdtGlyphEntry glyph)
-        {
-            return "sjis=0x" + glyph.ShiftJisValue.ToString("X4") +
-                ", size=" + glyph.Width.ToString() + "x" + glyph.Height.ToString() +
-                ", offset=" + glyph.OffsetX.ToString() + "/" + glyph.OffsetY.ToString();
-        }
-
-        private static Dictionary<string, byte[]> ReadAsciiKerningEntries(byte[] fdt)
-        {
-            Dictionary<string, byte[]> entries = new Dictionary<string, byte[]>(StringComparer.Ordinal);
-            if (fdt == null || fdt.Length < FdtHeaderSize)
-            {
-                return entries;
-            }
-
-            uint headerOffset = Endian.ReadUInt32LE(fdt, 0x0C);
-            if (headerOffset == 0 ||
-                headerOffset > int.MaxValue ||
-                headerOffset < FdtHeaderSize ||
-                headerOffset > fdt.Length - FdtKerningHeaderSize)
-            {
-                return entries;
-            }
-
-            int kerningHeaderOffset = checked((int)headerOffset);
-            if (!HasAsciiMagic(fdt, kerningHeaderOffset, "knhd"))
-            {
-                return entries;
-            }
-
-            uint kerningCount = Endian.ReadUInt32LE(fdt, kerningHeaderOffset + 0x04);
-            int kerningStart = kerningHeaderOffset + FdtKerningHeaderSize;
-            long kerningBytes = (long)kerningCount * FdtKerningEntrySize;
-            if (kerningBytes < 0 || kerningStart > fdt.Length || kerningStart + kerningBytes > fdt.Length)
-            {
-                return entries;
-            }
-
-            for (int i = 0; i < kerningCount; i++)
-            {
-                int offset = kerningStart + i * FdtKerningEntrySize;
-                uint left = Endian.ReadUInt32LE(fdt, offset);
-                uint right = Endian.ReadUInt32LE(fdt, offset + 4);
-                if (!IsAsciiKerningKey(left) || !IsAsciiKerningKey(right))
-                {
-                    continue;
-                }
-
-                byte[] entry = new byte[FdtKerningEntrySize];
-                Buffer.BlockCopy(fdt, offset, entry, 0, FdtKerningEntrySize);
-                entries[left.ToString("X2") + ":" + right.ToString("X2")] = entry;
-            }
-
-            return entries;
-        }
-
-        private static bool IsAsciiKerningKey(uint value)
-        {
-            return value >= 0x20 && value <= 0x7E;
-        }
-
-        private static bool BytesEqual(byte[] left, byte[] right)
-        {
-            if (ReferenceEquals(left, right))
-            {
-                return true;
-            }
-
-            if (left == null || right == null || left.Length != right.Length)
-            {
-                return false;
-            }
-
-            for (int i = 0; i < left.Length; i++)
-            {
-                if (left[i] != right[i])
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        private static bool TryFindGlyph(byte[] fdt, uint codepoint, out FdtGlyphEntry glyph)
-        {
-            int fontTableOffset;
-            uint glyphCount;
-            int glyphStart;
-            if (!TryGetFdtGlyphTable(fdt, out fontTableOffset, out glyphCount, out glyphStart))
-            {
-                glyph = new FdtGlyphEntry();
-                return false;
-            }
-
-            uint utf8Value = PackUtf8(codepoint);
-            for (int i = 0; i < glyphCount; i++)
-            {
-                int offset = glyphStart + i * FdtGlyphEntrySize;
-                if (Endian.ReadUInt32LE(fdt, offset) == utf8Value)
-                {
-                    glyph = new FdtGlyphEntry
-                    {
-                        ShiftJisValue = Endian.ReadUInt16LE(fdt, offset + 4),
-                        ImageIndex = Endian.ReadUInt16LE(fdt, offset + 6),
-                        X = Endian.ReadUInt16LE(fdt, offset + 8),
-                        Y = Endian.ReadUInt16LE(fdt, offset + 10),
-                        Width = fdt[offset + 12],
-                        Height = fdt[offset + 13],
-                        OffsetX = unchecked((sbyte)fdt[offset + 14]),
-                        OffsetY = unchecked((sbyte)fdt[offset + 15])
-                    };
-                    return true;
-                }
-            }
-
-            glyph = new FdtGlyphEntry();
-            return false;
-        }
-
-        private static bool TryGetFdtGlyphTable(byte[] fdt, out int fontTableOffset, out uint glyphCount, out int glyphStart)
-        {
-            fontTableOffset = 0;
-            glyphCount = 0;
-            glyphStart = 0;
-            if (fdt == null || fdt.Length < FdtHeaderSize + FdtFontTableHeaderSize)
-            {
-                return false;
-            }
-
-            uint fontTableHeaderOffset = Endian.ReadUInt32LE(fdt, 0x08);
-            if (fontTableHeaderOffset >= fdt.Length || fontTableHeaderOffset > int.MaxValue)
-            {
-                return false;
-            }
-
-            fontTableOffset = checked((int)fontTableHeaderOffset);
-            if (fontTableOffset + FdtFontTableHeaderSize > fdt.Length)
-            {
-                return false;
-            }
-
-            glyphCount = Endian.ReadUInt32LE(fdt, fontTableOffset + 0x04);
-            glyphStart = fontTableOffset + FdtFontTableHeaderSize;
-            long glyphBytes = (long)glyphCount * FdtGlyphEntrySize;
-            return glyphBytes >= 0 && glyphStart <= fdt.Length && glyphStart + glyphBytes <= fdt.Length;
-        }
-
-        private static uint PackUtf8(uint codepoint)
-        {
-            if (codepoint <= 0x7F)
-            {
-                return codepoint;
-            }
-
-            if (codepoint <= 0x7FF)
-            {
-                return (uint)((0xC0 | (codepoint >> 6)) << 8 | (0x80 | (codepoint & 0x3F)));
-            }
-
-            if (codepoint <= 0xFFFF)
-            {
-                return (uint)((0xE0 | (codepoint >> 12)) << 16 |
-                              (0x80 | ((codepoint >> 6) & 0x3F)) << 8 |
-                              (0x80 | (codepoint & 0x3F)));
-            }
-
-            return (uint)((0xF0 | (codepoint >> 18)) << 24 |
-                          (0x80 | ((codepoint >> 12) & 0x3F)) << 16 |
-                          (0x80 | ((codepoint >> 6) & 0x3F)) << 8 |
-                          (0x80 | (codepoint & 0x3F)));
         }
 
         private static string ResolveFontTexturePath(string fdtPath, int imageIndex)
@@ -467,18 +278,6 @@ namespace FfxivKoreanPatch.PatchRouteVerifier
                 ((uint)data[offset + 1] << 8) |
                 ((uint)data[offset + 2] << 16) |
                 ((uint)data[offset + 3] << 24);
-        }
-
-        private struct FdtGlyphEntry
-        {
-            public ushort ShiftJisValue;
-            public ushort ImageIndex;
-            public ushort X;
-            public ushort Y;
-            public byte Width;
-            public byte Height;
-            public sbyte OffsetX;
-            public sbyte OffsetY;
         }
 
         private struct Texture
