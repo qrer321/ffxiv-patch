cad3bff95155908ce7c23ade0d290a41aceadf13
qrer321 <85899638+qrer321@users.noreply.github.com>
2026-05-09 01:40:09 +0900

    refactor: split glyph dump formatting

--- Tools/PatchRouteVerifier/GlyphDumpOutput.cs
+++ Tools/PatchRouteVerifier/GlyphDumpOutput.cs
@@ -200,44 +200,6 @@ namespace FfxivKoreanPatch.PatchRouteVerifier
                 }
             }
 
-            private static string FormatBounds(GlyphStats stats)
-            {
-                if (stats.ComponentCount == 0)
-                {
-                    return "-";
-                }
-
-                return stats.MinX.ToString() + "," +
-                       stats.MinY.ToString() + "-" +
-                       stats.MaxX.ToString() + "," +
-                       stats.MaxY.ToString();
-            }
-
-            private static string SanitizeFileName(string value)
-            {
-                StringBuilder builder = new StringBuilder(value.Length);
-                for (int i = 0; i < value.Length; i++)
-                {
-                    char ch = value[i];
-                    if ((ch >= 'a' && ch <= 'z') ||
-                        (ch >= 'A' && ch <= 'Z') ||
-                        (ch >= '0' && ch <= '9'))
-                    {
-                        builder.Append(ch);
-                    }
-                    else
-                    {
-                        builder.Append('_');
-                    }
-                }
-
-                return builder.ToString().Trim('_');
-            }
-
-            private static string EscapeTsv(string value)
-            {
-                return (value ?? string.Empty).Replace("\t", " ").Replace("\r", "\\r").Replace("\n", "\\n");
-            }
         }
     }
 }
