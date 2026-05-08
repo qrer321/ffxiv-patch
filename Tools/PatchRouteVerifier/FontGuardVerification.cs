9bcb9af5e601ca72a6aac670d7f275e46a62365c
qrer321 <85899638+qrer321@users.noreply.github.com>
2026-05-09 02:51:09 +0900

    refactor: split font guard data

--- Tools/PatchRouteVerifier/FontGuardVerification.cs
+++ Tools/PatchRouteVerifier/FontGuardVerification.cs
@@ -9,16 +9,15 @@ namespace FfxivKoreanPatch.PatchRouteVerifier
             private void Verify4kLobbyFontDerivations()
             {
                 Console.WriteLine("[FDT] 4K lobby font derivations");
-                uint[] latinCodepoints = new uint[] { 'A', 'a', '0', '1' };
 
                 for (int i = 0; i < Derived4kLobbyFontPairs.GetLength(0); i++)
                 {
                     string targetFontPath = Derived4kLobbyFontPairs[i, 0];
                     string sourceFontPath = Derived4kLobbyFontPairs[i, 1];
 
-                    for (int latinIndex = 0; latinIndex < latinCodepoints.Length; latinIndex++)
+                    for (int latinIndex = 0; latinIndex < FourKLobbyLatinCodepoints.Length; latinIndex++)
                     {
-                        ExpectGlyphEqualIfSourceExists(_cleanFont, targetFontPath, latinCodepoints[latinIndex], _patchedFont, targetFontPath, latinCodepoints[latinIndex]);
+                        ExpectGlyphEqualIfSourceExists(_cleanFont, targetFontPath, FourKLobbyLatinCodepoints[latinIndex], _patchedFont, targetFontPath, FourKLobbyLatinCodepoints[latinIndex]);
                     }
 
                     byte[] sourceFdt;
@@ -92,68 +91,19 @@ namespace FfxivKoreanPatch.PatchRouteVerifier
             private void VerifyNumericGlyphs()
             {
                 Console.WriteLine("[FDT] Numeric glyphs used by duty/reward UI");
-                string[] sameFontChecks =
+                for (int f = 0; f < NumericGlyphSameFontChecks.Length; f++)
                 {
-                    "common/font/AXIS_12.fdt",
-                    "common/font/AXIS_14.fdt",
-                    "common/font/AXIS_12_lobby.fdt",
-                    "common/font/AXIS_14_lobby.fdt",
-                    "common/font/Jupiter_45.fdt",
-                    "common/font/Jupiter_45_lobby.fdt",
-                    "common/font/Jupiter_90.fdt",
-                    "common/font/Jupiter_90_lobby.fdt",
-                    "common/font/Jupiter_20_lobby.fdt",
-                    "common/font/Jupiter_23_lobby.fdt",
-                    "common/font/Jupiter_23.fdt",
-                    "common/font/Jupiter_46.fdt",
-                    "common/font/Jupiter_46_lobby.fdt",
-                    "common/font/Jupiter_16_lobby.fdt",
-                    "common/font/Meidinger_16.fdt",
-                    "common/font/Meidinger_16_lobby.fdt",
-                    "common/font/Meidinger_20.fdt",
-                    "common/font/Meidinger_20_lobby.fdt",
-                    "common/font/Meidinger_40.fdt",
-                    "common/font/Meidinger_40_lobby.fdt",
-                    "common/font/MiedingerMid_10.fdt",
-                    "common/font/MiedingerMid_10_lobby.fdt",
-                    "common/font/MiedingerMid_12.fdt",
-                    "common/font/MiedingerMid_12_lobby.fdt",
-                    "common/font/MiedingerMid_14.fdt",
-                    "common/font/MiedingerMid_14_lobby.fdt",
-                    "common/font/MiedingerMid_18.fdt",
-                    "common/font/MiedingerMid_18_lobby.fdt",
-                    "common/font/MiedingerMid_36.fdt",
-                    "common/font/MiedingerMid_36_lobby.fdt",
-                    "common/font/TrumpGothic_23.fdt",
-                    "common/font/TrumpGothic_23_lobby.fdt",
-                    "common/font/TrumpGothic_34.fdt",
-                    "common/font/TrumpGothic_34_lobby.fdt",
-                    "common/font/TrumpGothic_68.fdt",
-                    "common/font/TrumpGothic_68_lobby.fdt",
-                    "common/font/TrumpGothic_184.fdt",
-                    "common/font/TrumpGothic_184_lobby.fdt"
-                };
-                uint[] codepoints = { '0', '1', '2', '9' };
-                for (int f = 0; f < sameFontChecks.Length; f++)
-                {
-                    for (int c = 0; c < codepoints.Length; c++)
+                    for (int c = 0; c < NumericGlyphCodepoints.Length; c++)
                     {
-                        ExpectGlyphEqual(_cleanFont, sameFontChecks[f], codepoints[c], _patchedFont, sameFontChecks[f], codepoints[c]);
+                        ExpectGlyphEqual(_cleanFont, NumericGlyphSameFontChecks[f], NumericGlyphCodepoints[c], _patchedFont, NumericGlyphSameFontChecks[f], NumericGlyphCodepoints[c]);
                     }
                 }
 
-                string[,] krnFontChecks =
+                for (int f = 0; f < NumericGlyphKoreanFontChecks.GetLength(0); f++)
                 {
-                    { "common/font/AXIS_12.fdt", "common/font/KrnAXIS_120.fdt" },
-                    { "common/font/AXIS_14.fdt", "common/font/KrnAXIS_140.fdt" },
-                    { "common/font/AXIS_18.fdt", "common/font/KrnAXIS_180.fdt" },
-                    { "common/font/AXIS_36.fdt", "common/font/KrnAXIS_360.fdt" }
-                };
-                for (int f = 0; f < krnFontChecks.GetLength(0); f++)
-                {
-                    for (int c = 0; c < codepoints.Length; c++)
+                    for (int c = 0; c < NumericGlyphCodepoints.Length; c++)
                     {
-                        ExpectGlyphEqual(_cleanFont, krnFontChecks[f, 0], codepoints[c], _patchedFont, krnFontChecks[f, 1], codepoints[c]);
+                        ExpectGlyphEqual(_cleanFont, NumericGlyphKoreanFontChecks[f, 0], NumericGlyphCodepoints[c], _patchedFont, NumericGlyphKoreanFontChecks[f, 1], NumericGlyphCodepoints[c]);
                     }
                 }
             }
@@ -173,81 +123,38 @@ namespace FfxivKoreanPatch.PatchRouteVerifier
             private void VerifyProtectedHangulGlyphs()
             {
                 Console.WriteLine("[FDT] Protected Hangul glyphs");
-                uint[] codepoints =
+                for (int f = 0; f < ProtectedHangulFonts.Length; f++)
                 {
-                    0xD638, // ho
-                    0xD63C  // hon
-                };
-                string[] fonts =
-                {
-                    "common/font/AXIS_36.fdt",
-                    "common/font/MiedingerMid_36.fdt",
-                    "common/font/TrumpGothic_34.fdt"
-                };
-                for (int f = 0; f < fonts.Length; f++)
-                {
-                    for (int c = 0; c < codepoints.Length; c++)
+                    for (int c = 0; c < ProtectedHangulCodepoints.Length; c++)
                     {
-                        ExpectGlyphVisibleAtLeast(_patchedFont, fonts[f], codepoints[c], ProtectedHangulMinimumVisiblePixels);
-                        ExpectGlyphNotEqualToFallback(fonts[f], codepoints[c], '-');
-                        ExpectGlyphNotEqualToFallback(fonts[f], codepoints[c], '=');
+                        ExpectGlyphVisibleAtLeast(_patchedFont, ProtectedHangulFonts[f], ProtectedHangulCodepoints[c], ProtectedHangulMinimumVisiblePixels);
+                        ExpectGlyphNotEqualToFallback(ProtectedHangulFonts[f], ProtectedHangulCodepoints[c], '-');
+                        ExpectGlyphNotEqualToFallback(ProtectedHangulFonts[f], ProtectedHangulCodepoints[c], '=');
                     }
                 }
             }
 
             private void VerifyPartyListSelfMarkerGlyph(uint codepoint)
             {
-                string[] sameFontChecks =
+                for (int i = 0; i < PartyListSelfMarkerSameFontChecks.Length; i++)
                 {
-                    "common/font/AXIS_12.fdt",
-                    "common/font/AXIS_14.fdt",
-                    "common/font/AXIS_18.fdt",
-                    "common/font/AXIS_36.fdt",
-                    "common/font/MiedingerMid_10.fdt",
-                    "common/font/MiedingerMid_12.fdt",
-                    "common/font/MiedingerMid_14.fdt",
-                    "common/font/MiedingerMid_18.fdt",
-                    "common/font/MiedingerMid_36.fdt"
-                };
-                for (int i = 0; i < sameFontChecks.Length; i++)
-                {
-                    ExpectGlyphEqualIfSourceExists(_cleanFont, sameFontChecks[i], codepoint, _patchedFont, sameFontChecks[i], codepoint);
+                    ExpectGlyphEqualIfSourceExists(_cleanFont, PartyListSelfMarkerSameFontChecks[i], codepoint, _patchedFont, PartyListSelfMarkerSameFontChecks[i], codepoint);
                 }
 
-                string[,] krnFontChecks =
+                for (int i = 0; i < PartyListSelfMarkerKoreanFontChecks.GetLength(0); i++)
                 {
-                    { "common/font/AXIS_12.fdt", "common/font/KrnAXIS_120.fdt" },
-                    { "common/font/AXIS_14.fdt", "common/font/KrnAXIS_140.fdt" },
-                    { "common/font/AXIS_18.fdt", "common/font/KrnAXIS_180.fdt" },
-                    { "common/font/AXIS_36.fdt", "common/font/KrnAXIS_360.fdt" }
-                };
-                for (int i = 0; i < krnFontChecks.GetLength(0); i++)
-                {
-                    ExpectGlyphEqualIfSourceExists(_cleanFont, krnFontChecks[i, 0], codepoint, _patchedFont, krnFontChecks[i, 1], codepoint);
+                    ExpectGlyphEqualIfSourceExists(_cleanFont, PartyListSelfMarkerKoreanFontChecks[i, 0], codepoint, _patchedFont, PartyListSelfMarkerKoreanFontChecks[i, 1], codepoint);
                 }
             }
 
             private void VerifyLobbyHangulVisibility()
             {
                 Console.WriteLine("[FDT] Lobby Hangul visibility guard");
-                string[] fonts =
+                for (int f = 0; f < LobbyHangulVisibilityFonts.Length; f++)
                 {
-                    "common/font/AXIS_12_lobby.fdt",
-                    "common/font/AXIS_14_lobby.fdt"
-                };
-                uint[] codepoints =
-                {
-                    0xB85C, // ro
-                    0xC2A4, // seu
-                    0xAC00, // ga
-                    0xC774, // i
-                    0xC544  // a
-                };
-                for (int f = 0; f < fonts.Length; f++)
-                {
-                    for (int c = 0; c < codepoints.Length; c++)
+                    for (int c = 0; c < LobbyHangulVisibilityCodepoints.Length; c++)
                     {
-                        ExpectGlyphVisible(_patchedFont, fonts[f], codepoints[c]);
+                        ExpectGlyphVisible(_patchedFont, LobbyHangulVisibilityFonts[f], LobbyHangulVisibilityCodepoints[c]);
                     }
                 }
             }
---
+++ Tools/PatchRouteVerifier/FontGuardVerificationData.cs
@@ -0,0 +1,111 @@
+namespace FfxivKoreanPatch.PatchRouteVerifier
+{
+    internal static partial class PatchRouteVerifier
+    {
+        private sealed partial class Verifier
+        {
+            private static readonly uint[] FourKLobbyLatinCodepoints = new uint[] { 'A', 'a', '0', '1' };
+
+            private static readonly string[] NumericGlyphSameFontChecks = new string[]
+            {
+                "common/font/AXIS_12.fdt",
+                "common/font/AXIS_14.fdt",
+                "common/font/AXIS_12_lobby.fdt",
+                "common/font/AXIS_14_lobby.fdt",
+                "common/font/Jupiter_45.fdt",
+                "common/font/Jupiter_45_lobby.fdt",
+                "common/font/Jupiter_90.fdt",
+                "common/font/Jupiter_90_lobby.fdt",
+                "common/font/Jupiter_20_lobby.fdt",
+                "common/font/Jupiter_23_lobby.fdt",
+                "common/font/Jupiter_23.fdt",
+                "common/font/Jupiter_46.fdt",
+                "common/font/Jupiter_46_lobby.fdt",
+                "common/font/Jupiter_16_lobby.fdt",
+                "common/font/Meidinger_16.fdt",
+                "common/font/Meidinger_16_lobby.fdt",
+                "common/font/Meidinger_20.fdt",
+                "common/font/Meidinger_20_lobby.fdt",
+                "common/font/Meidinger_40.fdt",
+                "common/font/Meidinger_40_lobby.fdt",
+                "common/font/MiedingerMid_10.fdt",
+                "common/font/MiedingerMid_10_lobby.fdt",
+                "common/font/MiedingerMid_12.fdt",
+                "common/font/MiedingerMid_12_lobby.fdt",
+                "common/font/MiedingerMid_14.fdt",
+                "common/font/MiedingerMid_14_lobby.fdt",
+                "common/font/MiedingerMid_18.fdt",
+                "common/font/MiedingerMid_18_lobby.fdt",
+                "common/font/MiedingerMid_36.fdt",
+                "common/font/MiedingerMid_36_lobby.fdt",
+                "common/font/TrumpGothic_23.fdt",
+                "common/font/TrumpGothic_23_lobby.fdt",
+                "common/font/TrumpGothic_34.fdt",
+                "common/font/TrumpGothic_34_lobby.fdt",
+                "common/font/TrumpGothic_68.fdt",
+                "common/font/TrumpGothic_68_lobby.fdt",
+                "common/font/TrumpGothic_184.fdt",
+                "common/font/TrumpGothic_184_lobby.fdt"
+            };
+
+            private static readonly uint[] NumericGlyphCodepoints = new uint[] { '0', '1', '2', '9' };
+
+            private static readonly string[,] NumericGlyphKoreanFontChecks = new string[,]
+            {
+                { "common/font/AXIS_12.fdt", "common/font/KrnAXIS_120.fdt" },
+                { "common/font/AXIS_14.fdt", "common/font/KrnAXIS_140.fdt" },
+                { "common/font/AXIS_18.fdt", "common/font/KrnAXIS_180.fdt" },
+                { "common/font/AXIS_36.fdt", "common/font/KrnAXIS_360.fdt" }
+            };
+
+            private static readonly uint[] ProtectedHangulCodepoints = new uint[]
+            {
+                0xD638,
+                0xD63C
+            };
+
+            private static readonly string[] ProtectedHangulFonts = new string[]
+            {
+                "common/font/AXIS_36.fdt",
+                "common/font/MiedingerMid_36.fdt",
+                "common/font/TrumpGothic_34.fdt"
+            };
+
+            private static readonly string[] PartyListSelfMarkerSameFontChecks = new string[]
+            {
+                "common/font/AXIS_12.fdt",
+                "common/font/AXIS_14.fdt",
+                "common/font/AXIS_18.fdt",
+                "common/font/AXIS_36.fdt",
+                "common/font/MiedingerMid_10.fdt",
+                "common/font/MiedingerMid_12.fdt",
+                "common/font/MiedingerMid_14.fdt",
+                "common/font/MiedingerMid_18.fdt",
+                "common/font/MiedingerMid_36.fdt"
+            };
+
+            private static readonly string[,] PartyListSelfMarkerKoreanFontChecks = new string[,]
+            {
+                { "common/font/AXIS_12.fdt", "common/font/KrnAXIS_120.fdt" },
+                { "common/font/AXIS_14.fdt", "common/font/KrnAXIS_140.fdt" },
+                { "common/font/AXIS_18.fdt", "common/font/KrnAXIS_180.fdt" },
+                { "common/font/AXIS_36.fdt", "common/font/KrnAXIS_360.fdt" }
+            };
+
+            private static readonly string[] LobbyHangulVisibilityFonts = new string[]
+            {
+                "common/font/AXIS_12_lobby.fdt",
+                "common/font/AXIS_14_lobby.fdt"
+            };
+
+            private static readonly uint[] LobbyHangulVisibilityCodepoints = new uint[]
+            {
+                0xB85C,
+                0xC2A4,
+                0xAC00,
+                0xC774,
+                0xC544
+            };
+        }
+    }
+}
